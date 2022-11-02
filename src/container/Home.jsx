7382ab348cd30b7ba56fd95ba8059544fe57779e
khusidm <khusidasm7@gmail.com>
2022-11-02 23:46:55 +0530

    Footer added

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Spinner from '../components/Spinner'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 function Home() {
   return (
     <div>
     <Spinner />
+    <Footer />
     </div>
   )
 }
