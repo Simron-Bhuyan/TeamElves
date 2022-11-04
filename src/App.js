249e5735bed42b23869272d6c15357de45322cef
Simron <simron0707@gmail.com>
2022-11-04 23:42:42 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Home from './container/Home';
 import Checker from './container/Checker';
+import { memo } from "react";
+import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <Home/>
-    // <Checker/>
+    <Routes>
+        <Route path={"/*"} element={<Home />} />
+        <Route path={"/plagarism-checker"} element={<Checker/>} />
+      </Routes>
   )
 }
 
-export default App
+export default memo(App);
