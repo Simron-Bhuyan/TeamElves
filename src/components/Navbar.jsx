a8f870821bcac189c6655e0840e1662f33f7bffc
khusidm <khusidasm7@gmail.com>
2022-11-04 23:27:32 +0530

    dark mode logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,13 @@ function Navbar(props) {
       <div className="container-fluid w-full flex flex-wrap items-center sm:justify-around md:justify-between px-6">
       <div className="flex flex-wrap ">
         <h1 className={props.darkMode ? "text-neutral-900 font-semibold text-lg" : "text-stone-50 font-semibold text-lg"} >
-        <a className="flex items-center h-[28px] my-2 mx-24 mb-4 md:my-0 md:ml-2">
-                <img src="https://user-images.githubusercontent.com/89746011/199548760-e3011cf6-27cb-4a12-aebb-af1faf352175.png" className="h-[8em]" alt="Detecto Logo" />
-            </a>
+        <a className="flex items-center h-[28px] my-2 mx-24 mb-4 md:my-1 md:ml-2">
+          {
+            props.darkMode ? 
+            <img src="https://user-images.githubusercontent.com/89746011/200042768-4ce65221-00ea-4e4b-af89-5c243bc64f3a.png" className="h-[10rem]" alt="Detecto Logo" /> :
+            <img src="https://user-images.githubusercontent.com/89746011/199548760-e3011cf6-27cb-4a12-aebb-af1faf352175.png" className="h-[10rem]" alt="Detecto Logo" /> 
+          } 
+          </a> 
         </h1>
       </div>
       <div className={props.darkMode ? "text-neutral-900 grid grid-cols-4 place-content-between justify-self-end mr-4" : "text-stone-50 grid grid-cols-4 place-content-between justify-self-end mr-4"}>
