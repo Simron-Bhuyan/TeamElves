5d34c1377656af60f9117f906986431b0970dd74
padmakshimandal <84122297+padmakshimandal@users.noreply.github.com>
2022-11-05 11:35:14 +0530

    MembersCard Restored

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import YoutubeEmbed from './Youtube'
+import React from "react";
+import YoutubeEmbed from "./Youtube";
 function About() {
   return (
-    <div className="flex sm:flex-wrap flex-row mt-40">
-      <div className="flex h-[max-content] w-[60vw]  p-5">
+    <div className=" flex-row-reverse md:flex mt-40">
+      <div className=" flex items-center p-5">
+        <div>
+          <h3 className="font-bold text-3xl text-center">LOREM</h3>
+          <p className="text-lg text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            repellat fugiat sit minus, dignissimos, corrupti nisi fugit
+            explicabo modi obcaecati voluptates aut accusantium totam accusamus
+            quae, consequuntur dolore quo earum? Eum nemo nulla omnis
+            recusandae.
+          </p>
+        </div>
+      </div>
+      <div className="flex h-[max-content] w-[100vw]  p-5">
         <YoutubeEmbed embedId="rokGy0huYEA" />
       </div>
-      <div className="Content flex ">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-        ad blanditiis eius nulla ea pariatur enim labore obcaecati, dolores
-        accusantium dolorum, velit sunt nesciunt. At natus enim odit optio
-        commodi.
-      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -5,7 +5,7 @@ const YoutubeEmbed = ({ embedId }) => (
   <div className="video-responsive">
     <iframe
       width="800"
-      height="400"
+      height="600"
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
